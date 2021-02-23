ec544f24886f080b840423065a6944d8c833b8b3
Jham <academyyyy12@gmail.com>
2021-02-23 15:19:26 +0700

    feat(campaign): integrasi end point get all campaigns

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
     // Global page headers: https://go.nuxtjs.dev/config-head
     head: {
-        title: 'startup-backer',
+        title: 'startup-backer-frontend',
         meta: [
             { charset: 'utf-8' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -40,11 +40,39 @@ export default {
         '@nuxtjs/axios',
         // https://go.nuxtjs.dev/pwa
         '@nuxtjs/pwa',
+        //https://auth.nuxtjs.org/guide/setup/
+        '@nuxtjs/axios',
+        '@nuxtjs/auth-next'
     ],
 
     // Axios module configuration: https://go.nuxtjs.dev/config-axios
     axios: {
-        baseURL: 'http://http://localhost:8080/',
+        baseURL: 'http://localhost:8080',
+    },
+
+    auth: {
+        strategies: {
+            local: {
+                token: {
+                    property: 'token',
+                    // required: true,
+                    // type: 'Bearer'
+                },
+                user: {
+                    property: 'user',
+                    // autoFetch: true
+                },
+                endpoints: {
+                    login: { url: '/api/v1/sessions', method: 'post', propertyName: 'data.token' },
+                    logout: false,
+                    user: { url: '/api/v1/users/fetch', method: 'post', propertyName: 'data' }
+                },
+                // tokenRequired : true,
+                // tokenType : 'bearer',
+                // globalToken: true,
+                // autoFetchUser : true
+            }
+        }
     },
 
     // PWA module configuration: https://go.nuxtjs.dev/pwa
